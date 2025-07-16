94f8b88dcb0a3345b13dbbaf5c592bccf08b0632
Shanmin Zhang <zhangshanmin@xiaomi.com>
2025-07-17 01:48:18 +0800

    GCC: update to version to 13.4.0 (arm-none-eabi) 20250718
    Signed-off-by: Shanmin Zhang <zhangshanmin@xiaomi.com>

---
+++ lib/gcc/arm-none-eabi/13.4.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/shanmin/gcc-build/gcc/configure --prefix=/home/shanmin/gcc-build/out/linux-x86_64/arm-none-eabi --host=x86_64-unknown-linux-gnu --build=x86_64-unknown-linux-gnu --target=arm-none-eabi --with-zstd=/home/shanmin/gcc-build/out/linux-x86_64/arm-none-eabi --with-gmp=/home/shanmin/gcc-build/out/linux-x86_64/arm-none-eabi --with-mpfr=/home/shanmin/gcc-build/out/linux-x86_64/arm-none-eabi --with-mpc=/home/shanmin/gcc-build/out/linux-x86_64/arm-none-eabi --with-gnu-as --with-gnu-ld --disable-werror --disable-shared --disable-nls --disable-threads --disable-tls --with-system-zlib --enable-gnu-unique-object --enable-linker-build-id --with-newlib --with-sysroot=/home/shanmin/gcc-build/out/linux-x86_64/arm-none-eabi/arm-none-eabi --with-native-system-header-dir=/include --with-headers=yes --enable-languages=c,c++,lto --enable-multilib --with-multilib-list=aprofile,rmprofile --enable-checking=release";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm7tdmi" }, { "float", "soft" } };
